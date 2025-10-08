120c66eb81d10e5b2c5c2dd9c849a01f78581d0d
Byte-nana <nanadevelop44@gmail.com>
2025-10-08 21:42:34 +0900

    refactor:update variable for e.clientX and e.clientY

--- src/main.js
+++ src/main.js
@@ -6,9 +6,12 @@ const target = document.querySelector('.target');
 const tag = document.querySelector('.tag');
 
 document.addEventListener('mousemove', (e) => {
-  horizontal.style.transform = `translateY(${e.clientY}px)`;
-  vertical.style.transform = `translateX(${e.clientX}px)`;
-  target.style.transform = `translate(${e.clientX}px, ${e.clientY}px) translate(-50%, -50%)`;
-  tag.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
-  tag.innerHTML = `${e.clientX}px, ${e.clientY}px`;
+  const x = e.clientX;
+  const y = e.clientY;
+
+  horizontal.style.transform = `translateY(${y}px)`;
+  vertical.style.transform = `translateX(${x}px)`;
+  target.style.transform = `translate(${x}px, ${y}px) translate(-50%, -50%)`;
+  tag.style.transform = `translate(${x}px, ${y}px)`;
+  tag.innerHTML = `${x}px, ${y}px`;
 });
