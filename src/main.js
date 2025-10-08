cfcb9cc621dc8161b6548c83fbea1c72b141ec0b
Byte-nana <nanadevelop44@gmail.com>
2025-10-08 21:28:19 +0900

    feat:mousemove event

---
+++ src/main.js
@@ -0,0 +1,14 @@
+'use strict ';
+
+const horizontal = document.querySelector('.horizontal');
+const vertical = document.querySelector('.vertical');
+const target = document.querySelector('.target');
+const tag = document.querySelector('.tag');
+
+document.addEventListener('mousemove', (e) => {
+  horizontal.style.transform = `translateY(${e.clientY}px)`;
+  vertical.style.transform = `translateX(${e.clientX}px)`;
+  target.style.transform = `translate(${e.clientX}px, ${e.clientY}px) translate(-50%, -50%)`;
+  tag.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+  tag.innerHTML = `${e.clientX}, ${e.clientY}`;
+});
