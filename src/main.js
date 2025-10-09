84dd54ef3a214ce263a575b0999bf9a82d39caf9
Byte-nana <nanadevelop44@gmail.com>
2025-10-10 08:27:30 +0900

    refactor:secure load event and get width and height of the target image to translate

--- src/main.js
+++ src/main.js
@@ -5,13 +5,20 @@ const vertical = document.querySelector('.vertical');
 const target = document.querySelector('.target');
 const tag = document.querySelector('.tag');
 
-document.addEventListener('mousemove', (e) => {
-  const x = e.clientX;
-  const y = e.clientY;
+addEventListener('load', () => {
+  const targetHalfHeight = target.getBoundingClientRect().height / 2;
+  const targetHalfWidth = target.getBoundingClientRect().width / 2;
 
-  horizontal.style.transform = `translateY(${y}px)`;
-  vertical.style.transform = `translateX(${x}px)`;
-  target.style.transform = `translate(${x}px, ${y}px) translate(-50%, -50%)`;
-  tag.style.transform = `translate(${x}px, ${y}px)`;
-  tag.innerHTML = `${x}px, ${y}px`;
+  document.addEventListener('mousemove', (e) => {
+    const x = e.clientX;
+    const y = e.clientY;
+
+    horizontal.style.transform = `translateY(${y}px)`;
+    vertical.style.transform = `translateX(${x}px)`;
+    target.style.transform = `translate(${x - targetHalfWidth}px, ${
+      y - targetHalfHeight
+    }px)`;
+    tag.style.transform = `translate(${x}px, ${y}px)`;
+    tag.innerHTML = `${x}px, ${y}px`;
+  });
 });
